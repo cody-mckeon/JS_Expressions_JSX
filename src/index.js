cf7154fa36cea087dc57216776b387f9e6773416
cody-mckeon <codymckeon@gmail.com>
2021-06-03 15:17:10 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+const name = "Cody McKeon";
+const year = new Date().getFullYear();
 //It should display 2 paragraph HTML elements.
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
+
 //The paragraphs should say:
 //Created by YOURNAME.
 //Copyright CURRENTYEAR.
